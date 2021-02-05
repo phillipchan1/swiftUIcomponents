8d02b5d26d4444b33b8c2090946d42a53b8895d4
Phillip Chan <phil.chan@sce.com>
2021-02-05 10:27:12 -0800

    Add customizable

--- Sources/ReusuableButton/ReusuableButton.swift
+++ Sources/ReusuableButton/ReusuableButton.swift
@@ -9,18 +9,19 @@ import SwiftUI
 
 @available(iOS 13.0, macOS 10.15, *)
 public struct ReusuableButton: View {
-    var label: String = "DefaultButton Label"
+    var label: String?
     
-    public init() {
-        
+    public init(label: String) {
+        self.label = label
     }
     
-    public var body: some View {        
+    public var body: some View {
         Button(action: {
             print("buttonpressed")
         }, label: {
-            Text(label)
-                .foregroundColor(.yellow)
+            Text(self.label ?? "default value")
+                .foregroundColor(.white)
+                .background(Color.yellow)
                 .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealWidth: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, idealHeight: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
         })
     }
@@ -29,6 +30,6 @@ public struct ReusuableButton: View {
 @available(iOS 13.0, macOS 10.15, *)
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        ReusuableButton()
+        ReusuableButton(label: "myLabel")
     }
 }
